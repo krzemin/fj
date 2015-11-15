02ae84d5d73d618445c6765d9730000b490f363a
Piotr Krzemiński <pio.krzeminski@gmail.com>
2015-11-16 00:46:37 +0100

    provided comprehensive example with summable natural numbers, mappable and summable lists

---
+++ src/main/java/Examples.java
@@ -0,0 +1,97 @@
+
+
+class Summable<X extends Object> extends Object {
+    X plus(X other) { return other; }
+}
+
+class Nat extends Summable<Nat> {
+    Nat() { super(); }
+    Succ succ() { return new Succ(this); }
+}
+
+class Zero extends Nat {
+    Zero() { super(); }
+    public String toString() { return "Zero"; }
+}
+
+class Succ extends Nat {
+    Nat prev;
+    Succ(Nat prev) { super(); this.prev = prev; }
+    Nat plus(Nat n) {
+        return this.prev.plus(n.succ());
+    }
+    public String toString() { return "Succ(" + prev.toString() + ")"; }
+}
+
+class UnaryFunc<X extends Object, Y extends Object> extends Object {
+    Y ignored;
+    UnaryFunc(Y ignored) { super(); this.ignored = ignored; }
+    Y apply(X arg) {
+        return this.ignored;
+    }
+}
+
+class TwicePlus1 extends UnaryFunc<Nat, Nat> {
+    TwicePlus1(Nat ignored) { super(ignored); }
+
+    Succ apply(Nat n) { return n.plus(n).succ(); }
+}
+
+
+class List<X extends Summable<X>> extends Object {
+    List() { super(); }
+    X sum(X zero) { return zero; }
+    <Y extends Summable<Y>> List<Y> map(UnaryFunc<X, Y> f) {
+        return new Nil<Y>();
+    }
+}
+
+class Nil<X extends Summable<X>> extends List<X> {
+    Nil() { super(); }
+    public String toString() { return "Nil"; }
+}
+
+class Cons<X extends Summable<X>> extends List<X> {
+    X head;
+    List<X> tail;
+
+    Cons(X head, List<X> tail) { super(); this.head = head; this.tail = tail; }
+
+    X sum(X zero) { return this.tail.sum(zero).plus(head); }
+
+    <Y extends Summable<Y>> List<Y> map(UnaryFunc<X, Y> f) {
+        return new Cons<Y>(f.apply(head), this.tail.map(f));
+    }
+
+    public String toString() { return head.toString() + " :: " + tail.toString(); }
+}
+
+
+
+class Examples {
+
+    public static void main(String[] args) {
+        Nat zero = new Zero();
+        Nat one = new Succ(zero);
+        Nat two = new Succ(one);
+        Nat three = new Succ(two);
+
+        List<Nat> empty = new Nil<Nat>();
+        List<Nat> two_ = new Cons<Nat>(two, empty);
+        List<Nat> three_two_ = new Cons<Nat>(three, two_);
+
+        TwicePlus1 twicePlus1 = new TwicePlus1(zero);
+
+
+        System.out.println(one.plus(three));
+        System.out.println(three.plus(three));
+
+        System.out.println(three_two_);
+        System.out.println(three_two_.sum(zero));
+
+        System.out.println(three_two_.map(twicePlus1));
+        System.out.println(three_two_.map(twicePlus1).sum(new Zero()));
+
+
+    }
+}
---
+++ src/main/scala/fgj/examples/Lists.scala
@@ -0,0 +1,157 @@
+package fgj.examples
+
+import fgj.AST._
+import fgj.Aux._
+import fgj.Types._
+import erasure.Erasure._
+
+object Lists extends App {
+
+  val Summable = Class("Summable",
+    typeParams = List(BoundedParam(TypeVar("X"), ClassType("Object"))),
+    baseClass = ClassType("Object"),
+    methods = List(Method(
+        name = "plus",
+        typeParams = List(),
+        resultType = TypeVar("X"),
+        args = List(Argument("other", TypeVar("X"))),
+        body = Var("other")
+    ))
+  )
+
+  val Nat = Class("Nat", List(), ClassType("Summable", List(ClassType("Nat"))),
+    methods = List(
+      Method("succ", List(), ClassType("Succ"), List(),
+        New(ClassType("Succ"), List(Var("this"))))
+    )
+  )
+
+  val Zero = Class("Zero", List(), ClassType("Nat"))
+
+  val Succ = Class("Succ", List(), ClassType("Nat"),
+    fields = List(
+      Field("prev", ClassType("Nat"))
+    ),
+    methods = List(
+      Method("plus", List(), ClassType("Nat"), List(Argument("n", ClassType("Nat"))),
+        Invoke(FieldAccess(Var("this"), "prev"), "plus", List(), List(Invoke(Var("n"), "succ"))))
+    )
+  )
+
+  val UnaryFunc = Class("UnaryFunc",
+    List(
+      BoundedParam(TypeVar("X"), ClassType("Object")),
+      BoundedParam(TypeVar("Y"), ClassType("Object"))
+    ),
+    ClassType("Object"),
+    fields = List(Field("ignored", TypeVar("Y"))),
+    methods = List(
+      Method("apply", List(), TypeVar("Y"),
+        List(Argument("arg", TypeVar("X"))),
+        FieldAccess(Var("this"), "ignored"))
+    )
+  )
+
+  val TwicePlus1 = Class("TwicePlus1", List(),
+    ClassType("UnaryFunc", List(ClassType("Nat"), ClassType("Nat"))),
+    methods = List(
+      Method("apply", List(), ClassType("Succ"),
+        List(Argument("n", ClassType("Nat"))),
+        Invoke(Invoke(Var("n"), "plus", List(), List(Var("n"))), "succ")
+      )
+    )
+  )
+
+  val Lst = Class("List",
+    List(BoundedParam(TypeVar("X"), ClassType("Summable", List(TypeVar("X"))))),
+    ClassType("Object"),
+    methods = List(
+      Method("sum", List(), TypeVar("X"),
+        List(Argument("zero", TypeVar("X"))), Var("zero")),
+      Method("map",
+        List(BoundedParam(TypeVar("Y"), ClassType("Summable", List(TypeVar("Y"))))),
+        ClassType("List", List(TypeVar("Y"))),
+        List(Argument("f", ClassType("UnaryFunc", List(TypeVar("X"), TypeVar("Y"))))),
+        New(ClassType("Nil", List(TypeVar("Y"))))
+      )
+    )
+  )
+
+  val Nill = Class("Nil",
+    List(BoundedParam(TypeVar("X"), ClassType("Summable", List(TypeVar("X"))))),
+    ClassType("List", List(TypeVar("X")))
+  )
+
+  val Cons = Class("Cons",
+    List(BoundedParam(TypeVar("X"), ClassType("Summable", List(TypeVar("X"))))),
+    ClassType("List", List(TypeVar("X"))),
+    fields = List(
+      Field("head", TypeVar("X")),
+      Field("tail", ClassType("List", List(TypeVar("X"))))
+    ),
+    methods = List(
+      Method("sum", List(), TypeVar("X"),
+        List(Argument("zero", TypeVar("X"))),
+        Invoke(
+          Invoke(FieldAccess(Var("this"), "tail"), "sum", List(), List(Var("zero"))),
+          "plus",
+          List(),
+          List(FieldAccess(Var("this"), "head"))
+        )
+      ),
+      Method("map",
+        List(BoundedParam(TypeVar("Y"), ClassType("Summable", List(TypeVar("Y"))))),
+        ClassType("List", List(TypeVar("Y"))),
+        List(Argument("f", ClassType("UnaryFunc", List(TypeVar("X"), TypeVar("Y"))))),
+        New(ClassType("Cons", List(TypeVar("Y"))), List(
+          Invoke(Var("f"), "apply", List(), List(FieldAccess(Var("this"), "head"))),
+          Invoke(FieldAccess(Var("this"), "tail"), "map", List(), List(Var("f")))
+        ))
+      )
+    )
+  )
+
+
+
+  val ct = buildClassTable(List(Summable, Nat, Zero, Succ, UnaryFunc, TwicePlus1, Lst, Nill, Cons ))
+
+  ct.values.foreach(println)
+
+  ct.values.foreach { clz =>
+    println(s"does class ${clz.name} type? -> ${classTypes(clz.name)(ct, Map.empty, Map.empty)}")
+  }
+
+
+  val zero = New(ClassType("Zero"))
+  val one = New(ClassType("Succ"), List(zero))
+  val two = New(ClassType("Succ"), List(one))
+  val three = New(ClassType("Succ"), List(two))
+
+  val empty = New(ClassType("Nil", List(ClassType("Nat"))))
+  val two_ = New(ClassType("Cons", List(ClassType("Nat"))), List(two, empty))
+  val three_two_ = New(ClassType("Cons", List(ClassType("Nat"))), List(three, two_))
+
+  val twicePlus1 = New(ClassType("TwicePlus1"), List(zero))
+
+  val p1 = Program(ct, three_two_)
+  println(programType(p1))
+
+  val p1e = eraseProgram(p1)
+  p1e.classTable.values.foreach(println)
+  println(fj.Types.programType(p1e))
+  println(fj.Eval.evalProg(p1e))
+
+
+  val mapped = Invoke(three_two_, "map", List(ClassType("Nat")), List(twicePlus1))
+
+  val p2 = Program(ct, mapped)
+  val p2e = eraseProgram(p2)
+  println(fj.Types.programType(p2e))
+  println(fj.Eval.evalProg(p2e))
+
+  val p3 = Program(ct, Invoke(mapped, "sum", List(), List(zero)))
+  val p3e = eraseProgram(p3)
+  println(fj.Types.programType(p3e))
+  println(fj.Eval.evalProg(p3e))
+
+}
